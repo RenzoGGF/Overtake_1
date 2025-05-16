5f650ac2deeffb2eaf58f4ea3ff125228004e8f1
RenzoGGF <renzogf64@hotmail.com>
2025-05-15 20:37:49 -0500

    Botones de mostrar tablas

--- script.js
+++ script.js
@@ -69,3 +69,32 @@ function mostrarFormulario(tabla) {
           .then(data => contenido.innerHTML = `<p>${data}</p>`);
     });
 }
+
+function mostrarTabla(tabla) {
+    const contenido = document.getElementById("contenido");
+
+    fetch(`obtener_tabla.php?tabla=${tabla}`)
+        .then(response => response.json())
+        .then(data => {
+            let html = `<h2>${tabla}</h2><table border='1'><tr>`;
+
+            // Agregar nombres de columnas
+            Object.keys(data[0]).forEach(columna => {
+                html += `<th>${columna}</th>`;
+            });
+            html += `</tr>`;
+
+            // Agregar filas con datos
+            data.forEach(fila => {
+                html += `<tr>`;
+                Object.values(fila).forEach(valor => {
+                    html += `<td>${valor}</td>`;
+                });
+                html += `</tr>`;
+            });
+
+            html += `</table>`;
+            contenido.innerHTML = html;
+        })
+        .catch(() => contenido.innerHTML = "<p>Error al obtener datos.</p>");
+}
